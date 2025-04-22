07fb67816994e5fc57c9a5499124a2f9c5b9cdae
G0tYou <fanzdare@gmail.com>
2025-04-22 15:47:26 +0700

    add routes for limmitype

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.5
 require github.com/spf13/viper v1.20.1
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.2 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- limittype/delivery/http/handler.go
+++ limittype/delivery/http/handler.go
@@ -1,1 +1,46 @@
 package http
+
+import (
+	d "app/domain"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type ResponseError struct {
+	Message string `json:"message"`
+}
+
+type LimitTypeHandler struct {
+	slt d.ServiceLimitType
+}
+
+func NewLimitTypeHandler(e *echo.Echo, slt d.ServiceLimitType) {
+	handler := &LimitTypeHandler{slt}
+	//adding
+	e.POST("/limittype", handler.AddLimitType)
+	//listing
+	e.GET("/limittypes", handler.GetLimitTypes)
+}
+
+func (h *LimitTypeHandler) AddLimitType(c echo.Context) error {
+	var lt d.LimitType
+	if err := c.Bind(&lt); err != nil {
+		return c.JSON(http.StatusBadRequest, ResponseError{Message: http.StatusText(http.StatusBadRequest)})
+	}
+	_, err := h.slt.AddLimitType(c.Request().Context(), &lt)
+	fmt.Println(err)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+	}
+	return c.JSON(http.StatusCreated, http.StatusText(http.StatusCreated))
+}
+
+func (h *LimitTypeHandler) GetLimitTypes(c echo.Context) error {
+	lts, err := h.slt.GetLimitTypes(c.Request().Context())
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, ResponseError{Message: http.StatusText(http.StatusInternalServerError)})
+	}
+	return c.JSON(http.StatusOK, lts)
+}
